0214b28c71b8fafcaa895fe02ef956c4d6b6e937
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-09-21 20:17:31 +0200

    Update example

--- tdlib-futures/examples/test.rs
+++ tdlib-futures/examples/test.rs
@@ -1,27 +1,57 @@
-extern crate tdlib_futures;
+extern crate dotenv;
 extern crate futures;
-extern crate tokio_core;
-#[macro_use]
 extern crate serde_json;
+extern crate tdlib_futures;
+extern crate tokio_core;
 
+use dotenv::dotenv;
+use futures::prelude::*;
+use tdlib_futures::client::AuthParameters;
 use tdlib_futures::client::Client;
 use tdlib_futures::types::*;
-use futures::prelude::*;
 
 fn main() {
+    dotenv().ok();
+    tdlib_futures::set_log_verbosity_level(1);
+
     let mut core = tokio_core::reactor::Core::new().unwrap();
     let handle = core.handle();
 
     let mut client = Client::new();
-    let auth = client.authorize(handle.clone()).unwrap();
-
-    let chat_id = 11111111;
+    let getcode = || {
+        println!("getcode:");
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).expect("no input");
+        line.trim().to_owned()
+    };
+    let tdlib = TdlibParameters {
+        use_test_dc: false,
+        database_directory: "db".to_owned(),
+        files_directory: "Files".to_owned(),
+        use_file_database: true,
+        use_chat_info_database: true,
+        use_message_database: true,
+        use_secret_chats: false,
+        api_id: std::env::var("TDLIB_API_ID").unwrap().parse().unwrap(),
+        api_hash: std::env::var("TDLIB_API_HASH").unwrap().to_owned(),
+        system_language_code: "en".to_owned(),
+        device_model: "Desktop".to_owned(),
+        system_version: "Unknown".to_owned(),
+        application_version: "1.0".to_owned(),
+        enable_storage_optimizer: true,
+        ignore_file_names: false,
+    };
+    let params = AuthParameters {
+        tdlib,
+        encryption_key: std::env::var("TDLIB_ENCRYPTION_KEY").unwrap().to_owned(),
+        phone: std::env::var("TDLIB_PHONE").unwrap().to_owned(),
+        getcode,
+    };
+    let auth = client.authorize(handle.clone(), params).unwrap();
+    let chat_id = std::env::var("TG_USER").unwrap().parse().unwrap();
     let updates = auth.and_then(|updater| {
         let content = InputMessageText {
-            text: FormattedText {
-                text: "test".to_owned(),
-                entities: Vec::new(),
-            },
+            text: "test".to_owned(),
             disable_web_page_preview: false,
             clear_draft: false,
         };
@@ -33,12 +63,13 @@ fn main() {
             reply_markup: None,
             input_message_content: InputMessageContent::InputMessageText(content),
         };
-        client.send_spawn(msg, &handle);
+        let msg = client.send(msg).and_then(|r| {
+            println!("response: {:?}",r);
+            Ok(())
+        }).map_err(|_|());
+        handle.spawn(msg);
         updater.for_each(|u| {
-            match u {
-                // handle updates
-                _ => {}
-            }
+            println!("new update: {:?}",u);
             Ok(())
         })
     });
